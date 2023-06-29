a7d2d1f7704f4f1145534037bfd78ec6cd7b057a
chiranjeevi gurrapu <117752857+chiranjeevi3398@users.noreply.github.com>
2023-06-29 11:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 for(let i=0; i<str.lenght:i++){
-		if(lookup[str[i]]==undefined)
+		if(lookup[str[i]]==undefined){
 			decodedArr.push(str[i])
 		else decodedArr.push(lookup[str[i]])
 }
